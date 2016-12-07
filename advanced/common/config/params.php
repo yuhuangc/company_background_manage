bd53d911946c78b64e275360ff5cf73a7f78b25e
huangy02 <huangy02@mysoft.com.cn>
2016-12-07 17:50:38 +0800

    添加phpQuery

--- advanced/common/config/params.php
+++ advanced/common/config/params.php
@@ -1,6 +1,6 @@
 <?php
 return [
-    'adminEmail' => 'admin@example.com',
-    'supportEmail' => 'support@example.com',
+    'adminEmail' => '443663695@qq.com',
+    'supportEmail' => '443663695@qq.com',
     'user.passwordResetTokenExpire' => 3600,
 ];
---
+++ advanced/frontend/controllers/WebSiteCollectionController.php
@@ -0,0 +1,53 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: huangy02
+ * Date: 2016-12-6
+ * Time: 15:22
+ */
+
+namespace frontend\controllers;
+
+require('../../vendor/phpQuery/phpQuery.php');
+
+use phpQuery;
+use Yii;
+use yii\web\Controller;
+
+class WebSiteCollectionController extends Controller{
+
+    public function actionGet163News()
+    {
+        phpQuery::newDocumentFile('http://news.163.com');
+        $title = pq(".mod_important_news.none h5")->text();
+        $list = pq(".mod_important_news.none");
+        $list = $list->find('ul>li');
+        $content = '<h1>标题：'.$title."<br/></h1>";
+        foreach($list as $item){
+            $content .= $item->textContent.'<br/>';
+            $content .= pq($item)->find('a')->attr('href')."<br>";
+            $content .= "------------------------------------------------------"."<br>";
+        }
+        /*$date = date('Y-m-d',time());
+        $p = fopen($date.'.html', 'a');
+        fwrite($p, $content);
+        fclose($p);*/
+
+        /*$mail= Yii::$app->mailer->compose();
+        $mail->setTo('443663695@qq.com');
+        $mail->setSubject("邮件测试");
+        //$mail->setTextBody('zheshisha ');   //发布纯文字文本
+        $mail->setHtmlBody($content);    //发布可以带html标签的文本
+        if($mail->send())
+            echo "success";
+        else
+            echo "failse";
+        die();*/
+    }
+
+    public function actionGetSinaNews()
+    {
+        phpQuery::newDocumentFile('http://news.sina.cn/?r=nc&vt=4');
+        $title = pq(".mod_important_news.none h5")->text();
+    }
+}
